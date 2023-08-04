decfdb1ac59dea950fccb19597013bd051df0ebd
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-08-04 15:39:56 -0400

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Col } from 'antd'
+import { Searcher } from './components/Searcher'
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Col span={8} offset={8}>
+          <Searcher />
+        </Col>
+      </div>
+    </>
+  )
+}
+
+export default App
